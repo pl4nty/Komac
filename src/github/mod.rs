37e7ae7527fa300ab5554a492a034c17eee68a0b
Tom Plant <tom@tplant.com.au>
2025-04-16 14:01:57 +0000

    Use custom winget-pkgs
    Signed-off-by: GitHub <noreply@github.com>

--- src/github/mod.rs
+++ src/github/mod.rs
@@ -9,5 +9,5 @@ pub use error::GitHubError;
 
 pub const MICROSOFT: &str = "microsoft";
 pub const WINGET_PKGS: &str = "winget-pkgs";
-pub const WINGET_PKGS_FULL_NAME: &str = formatcp!("{MICROSOFT}/{WINGET_PKGS}");
+pub const WINGET_PKGS_FULL_NAME: &str = "pl4nty/winget-pkgs-selfhost";
 pub const GITHUB_HOST: &str = "github.com";
